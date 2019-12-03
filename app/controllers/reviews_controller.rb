d03813b62a1718430aba75f7439dd081db438724
Melissa Robbins <melissamrobbins@gmail.com>
2019-12-03 16:01:39 -0700

    Add new and create methods to ReviewsController

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,17 @@
 class ReviewsController < ApplicationController
-  def show
-    @reviews = Review.where(shelter_id: params[:id])
+
+  def new
+    @shelter = Shelter.find(params[:shelter_id])
   end
 
+  def create
+    @shelter = Shelter.find(params[:shelter_id])
+    review = @shelter.reviews.create(review_params)
+    redirect_to "/shelters/#{@shelter.id}"
+  end
+
+private
+  def review_params
+    params.permit(:title, :rating, :content, :picture)
+  end
 end
