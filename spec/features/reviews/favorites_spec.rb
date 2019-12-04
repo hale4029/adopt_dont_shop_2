697ab8e534442a4cda14dc1b31ee1c182b480b2a
Harrison Levin <harrison483@gmail.com>
2019-12-04 12:38:05 -0700

    updated testfor story 8

--- spec/features/reviews/favorites_spec.rb
+++ spec/features/reviews/favorites_spec.rb
@@ -6,21 +6,15 @@ describe "favorites", type: :feature do
     find_link("Favorites: 0").visible?
     visit "/shelters"
     find_link("Favorites: 0").visible?
+    visit "/shelters/#{@shelter_1.id}/pets"
+    find_link("Favorites: 0").visible?
 
-    within "#shleter-#{@shelter_1.id}" do
-      find_link("Favorite").click
-    end
+    find_link("Favorite").click
 
     expect(page).to have_current_path("/shelters")
     find_link("Favorites: 1").visible?
 
-    within "#shleter-#{@shelter_2.id}" do
-      find_link("Favorite").click
-    end
-
-    expect(page).to have_current_path("/shelters")
-    find_link("Favorites: 2").visible?
   end
 
-  
+
 end
