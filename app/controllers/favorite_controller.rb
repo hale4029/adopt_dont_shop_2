28590e4ebbbea65a1e94946dc8964069b42f93e0
Harrison Levin <harrison483@gmail.com>
2019-12-05 11:57:11 -0700

    updates to favorite naming session

--- app/controllers/favorite_controller.rb
+++ app/controllers/favorite_controller.rb
@@ -3,7 +3,7 @@ class FavoriteController < ApplicationController
   def update
     pet = Pet.find(params[:id])
     @favorites.add_pet(pet.id)
-    session[:favorite] = @favorites.contents
+    session[:favorites] = @favorites.contents
     flash[:notice] = "#{pet.name} added to your favorites!"
     redirect_to "/pets/#{pet.id}"
   end
