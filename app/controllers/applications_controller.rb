38d5b4dfb48060d30339c539703f7c337c55d051
Melissa Robbins <melissamrobbins@gmail.com>
2019-12-07 13:38:51 -0700

    Add create method to ApplicationsController
    Co-Authored-By: Harrison <harrison483@gmail.com>

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -4,4 +4,20 @@ class ApplicationsController < ApplicationController
     @pets = @favorites.favorite_pets
   end
 
+  def create
+    app = Application.new(app_params)
+    app.save
+    redirect_to "/favorites"
+  end
+
+  private
+    def app_params
+      params.permit(:name,
+                    :address,
+                    :city,
+                    :state,
+                    :zip,
+                    :phone,
+                    :description)
+    end
 end
