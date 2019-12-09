e8b07f03b3ae23f66e2cc029460cbaad3e9a9fce
Melissa Robbins <melissamrobbins@gmail.com>
2019-12-09 13:51:46 -0700

    Fix spacing
    Co-Authored-By: Harrison <harrison483@gmail.com>

--- app/models/application.rb
+++ app/models/application.rb
@@ -11,33 +11,33 @@ class Application < ApplicationRecord
                         :description,
                         :status
 
-def update_adoption_status(pets)
-  pets.each do |pet|
-    if pet.adoption_status == "adoptable"
-      status = "Pending Adoption"
-    else
-      status = "Adoptable"
+  def update_adoption_status(pets)
+    pets.each do |pet|
+      if pet.adoption_status == "adoptable"
+        status = "Pending Adoption"
+      else
+        status = "Adoptable"
+      end
+      pet.update({adoption_status: status})
+      pet.save
     end
-    pet.update({adoption_status: status})
-    pet.save
   end
-end
 
-def self.find_pets_in_applications
-  apps = Application.all
-  apps.map { |app| app.pets }.flatten
-end
+  def self.find_pets_in_applications
+    apps = Application.all
+    apps.map { |app| app.pets }.flatten
+  end
 
-def self.find_pets(ids)
-  ids.map { |id| Pet.find(id) }
-end
+  def self.find_pets(ids)
+    ids.map { |id| Pet.find(id) }
+  end
 
-def self.remove_all_pets_from_favorites(pets, favorites)
-  pets.each { |pet| favorites.remove_pet(pet.id) }
-end
+  def self.remove_all_pets_from_favorites(pets, favorites)
+    pets.each { |pet| favorites.remove_pet(pet.id) }
+  end
 
-def self.find_applicant(pet)
-  Application.select(:name, :id).joins(:pets).where("pets.id = #{pet.id}")
-end
+  def self.find_applicant(pet)
+    Application.select(:name, :id).joins(:pets).where("pets.id = #{pet.id}")
+  end
 
 end
