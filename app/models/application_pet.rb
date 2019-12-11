ad8b8b9beb753cf5833b9f76f9de883a24a91b04
Melissa Robbins <melissamrobbins@gmail.com>
2019-12-11 13:45:06 -0700

    Add css container

--- app/models/application_pet.rb
+++ app/models/application_pet.rb
@@ -6,12 +6,11 @@ class ApplicationPet < ApplicationRecord
                         :pet_id,
                         :application_id
 
-def self.approve_status
-  update({status: "Approved"})
-end
-
-def self.pending_status
-  update({status: "Pending"})
-end
-
+  def self.approve_status(pet_id, app_id)
+    where("pet_id = #{pet_id} AND application_id = #{app_id}").update({status: "Approved"})
+  end
+  
+  def self.pending_status(pet_id, app_id)
+    where("pet_id = #{pet_id} AND application_id = #{app_id}").update({status: "Pending"})
+  end
 end
