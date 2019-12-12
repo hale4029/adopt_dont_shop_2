c81aa1fd83f5503d0a9a17a1e1e756b0105c7717
Harrison Levin <harrison483@gmail.com>
2019-12-11 17:12:15 -0700

    favorites pets model test

--- spec/models/favorite_spec.rb
+++ spec/models/favorite_spec.rb
@@ -9,9 +9,7 @@ RSpec.describe Favorite do
         '2' => 1
       })
       expect(favorites.total_count).to eq(2)
-
       favorites.add_pet(3)
-
       expect(favorites.contents).to eq({'1' => 1, '2' => 1, '3' => 1})
     end
 
@@ -21,13 +19,52 @@ RSpec.describe Favorite do
         '2' => 1
       })
       expect(favorites.total_count).to eq(2)
-
       favorites.add_pet(1)
       favorites.add_pet(2)
-
       expect(favorites.contents).to eq({'1' => 1, '2' => 1})
     end
 
-  end
+    it "remove pet" do
+      favorites = Favorite.new({
+        '1' => 1,
+        '2' => 1
+      })
+      favorites.remove_pet(3)
+      expect(favorites.contents).to eq({'1' => 1, '2' => 1})
+    end
 
+    it "favorites pets" do
+      shelter_1 = Shelter.create(name: "Save Cats",
+                                address: "123 Pine",
+                                city: "Denver",
+                                state: "Colorado",
+                                zip: 80112)
+
+      pet_1 = shelter_1.pets.create(image: 'https://d17fnq9dkz9hgj.cloudfront.net/breed-uploads/2018/09/dog-landing-hero-lg.jpg?bust=1536935129&width=1080',
+                          name: "Jersey",
+                          approximate_age: "10",
+                          sex: "Male")
+      pet_2 = shelter_1.pets.create(image: 'https://d17fnq9dkz9hgj.cloudfront.net/breed-uploads/2018/09/dog-landing-hero-lg.jpg?bust=1536935129&width=1080',
+                          name: "Hershey",
+                          approximate_age: "10",
+                          sex: "Female")
+
+      favorites = Favorite.new({
+        pet_1.id => 1,
+        pet_2.id => 1
+      })
+      expect(favorites.total_count).to eq(2)
+      result = favorites.favorite_pets
+      expect(result).to eq([pet_1, pet_2])
+    end
+
+    it "remove all" do
+    end
+
+    it "find_pending_adoption_pets" do
+    end
+
+    it "find_approved_adoption_pets" do
+    end
+  end
 end
